dde1a530f433f516ec5e6d35a9d51f0aa72ac2bc
mango <ding.luo@dinglicom.com>
2018-12-05 20:05:52 +0800

    add module of timer test

---
+++ timer_test/timer_test.c
@@ -0,0 +1,37 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL");
+
+static void count_timer_handler(struct timer_list *timer);
+
+#if 0
+static struct timer_list count_timer;
+#else
+static DEFINE_TIMER(count_timer, count_timer_handler);
+#endif
+static int count = 0;
+
+static void count_timer_handler(struct timer_list *timer)
+{
+	mod_timer(&count_timer, jiffies + HZ);
+	printk(KERN_ALERT "count=%d\n", count);
+	count++;
+}
+
+static int __init timer_test_init(void)
+{
+	/*timer_setup(&count_timer, count_timer_handler, 0);*/
+	add_timer(&count_timer);
+	return 0;
+}
+
+static void __exit timer_test_exit(void)
+{
+    del_timer(&count_timer);
+	printk(KERN_ALERT "%s\n", __FUNCTION__);
+}
+
+module_init(timer_test_init);
+module_exit(timer_test_exit);
