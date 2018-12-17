6437ec0a7af99fcc5bd162449e38715e22414ab4
mango <ding.luo@dinglicom.com>
2018-12-17 16:52:56 +0800

    improve module of multi objects

--- multi_obj/hello.c
+++ multi_obj/hello.c
@@ -5,27 +5,9 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 extern void hello2_test(void);
 
-static char *name = "world";
-static int count = 2;
-static int index[8] = {};
-static int n_index = sizeof(index) / sizeof(index[0]);
-
-module_param(name, charp, S_IRUGO);
-module_param(count, int, S_IRUGO);
-module_param_array(index, int, &n_index, S_IRUGO);
-
 static int __init hello_init(void)
 {
-    int i;
-
-	printk(KERN_ALERT "hello, name=%s, count=%d, n_index=%d\n",
-			name, count, n_index);
-	
-	for (i = 0; i < n_index; i++)
-	{
-		printk(KERN_ALERT "index[%d]=%d\n", i, index[i]);
-	}
-
+	printk(KERN_ALERT "hello, test\n");
 	hello2_test();
 
 	return 0;
@@ -33,7 +15,7 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "goodbye, name=%s\n", name);
+	printk(KERN_ALERT "hello, goodbye\n");
 }
 
 module_init(hello_init);
--- multi_obj/readme.txt
+++ multi_obj/readme.txt
@@ -1,27 +1,6 @@
-linux驱动: 如何向模块传递参数, module_param和module_param_array
-https://www.cnblogs.com/mylinux/p/5670279.html
-
-insmod 内核模块参数传递
-https://www.cnblogs.com/lengbingshy/p/3909167.html
-
-用默认值加载ko
-sudo insmod hello.ko
-hello, name=world, count=2, n_index=8
-index[0]=0
-index[1]=0
-index[2]=0
-index[3]=0
-index[4]=0
-index[5]=0
-index[6]=0
-index[7]=0
+sudo insmod multi_obj.ko
+hello, test
 hello2 test
-goodbye, name=world
 
-用入参值加载ko
-sudo insmod hello.ko name="wahaha" count="13" index=00,10,011
-hello, name=wahaha, count=13, n_index=3
-index[0]=0
-index[1]=10
-index[2]=9
-goodbye, name=wahaha
+sudo rmmod multi_obj
+hello, goodbye
